55008b5ce58452eb052a1170ac6b231267bb324b
Szymon <szymonqqaz22@gmail.com>
2020-10-01 14:57:02 +0200

    Update python18.py

--- python18.py
+++ python18.py
@@ -4,25 +4,50 @@
 
 import random
 
-def checkTheCodes(mainCode, userCode):
+def checkTheCodesIsEqual(mainCode, userCode):
 
     isTheSame = True
 
     for index in range(0,4):
-        if mainCode[index] !== userCode[index]:
+        if mainCode[index] != userCode[index]:
             isTheSame = False
         
     return isTheSame
+
+def checkDifferences(mainCode, userCode):
+  bullsNumber = 0
+  cowsNumber = 0
+
+  for index in range(0,4):
+    if mainCode[index] == userCode[index]:
+      cowsNumber += 1
+
+  for mainCodeElement in mainCode:
+    for userCodeElement in userCode:
+      if userCodeElement == mainCodeElement:
+        bullsNumber += 1
+  
+  return bullsNumber
+  
+  # bullsNumber = bullsNumber - cowsNumber
+
 
 # userCode = input('Wpisz 4 cyfrowy kod rozdzielony ","')
 
 mainCode = []
 
+testCode = []
+
+# the bad way to create this like that because the numbers are repeatable
+
 for n in range(0, 4):
     mainCode.append(random.randint(0,9))
 
-    
+for n in range(0, 4):
+    testCode.append(random.randint(0,9))
 
-
-# def askUserAboutNumbers():
-    
+resoult = checkDifferences(mainCode, testCode)
+print(mainCode)
+print(testCode)
+print('-------')
+print(resoult)
